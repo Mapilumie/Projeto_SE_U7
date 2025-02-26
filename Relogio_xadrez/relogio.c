5d9cefaf8a434c1974da2d5387b461da58bf3475
Mapilumie <mapaulaferraz@gmail.com>
2025-02-25 21:54:43 -0300

    Organização das interações dentro do while(true) em funções separadas

--- Relogio_xadrez/relogio.c
+++ Relogio_xadrez/relogio.c
@@ -153,6 +153,148 @@ void atualiza_display(){
     render_on_display(ssd, &frame_area);
 }
 
+void verifica_botoes(bool a_apertado, bool b_apertado, bool joystick_apertado){
+    if(a_apertado == false){ // Se o botão A foi apertado
+        sleep_ms(10); // Tempo de debounce
+        if(a_apertado == false){
+            pausado = false;
+            timerA_correndo = false;
+            timerB_correndo = true;
+            gpio_put(LED_R_PIN, 0);
+            gpio_put(LED_G_PIN, 0);
+            gpio_put(LED_B_PIN, 1);
+        }
+    }
+    else if(b_apertado == false){
+        sleep_ms(10); // Tempo de debounce
+        if(b_apertado == false){
+            pausado = false;
+            timerA_correndo = true;
+            timerB_correndo = false;
+            gpio_put(LED_R_PIN, 0);
+            gpio_put(LED_G_PIN, 1);
+            gpio_put(LED_B_PIN, 0);
+        }
+    }
+    else if(joystick_apertado == false && pausado == false){
+        sleep_ms(10); // Tempo de debounce
+        if(joystick_apertado == false){
+            pausado = true;
+            timerA_correndo = false;
+            timerB_correndo = false;
+            gpio_put(LED_R_PIN, 0);
+            gpio_put(LED_G_PIN, 0);
+            gpio_put(LED_B_PIN, 0);
+        }
+    }
+    else if(joystick_apertado == false && pausado == true){ // Se o joystick foi apertado com o tempo pausado, o tempo é reiniciado;
+        sleep_ms(10); // Tempo de debounce
+        if(joystick_apertado == false && pausado == true){ 
+            timerA = TEMPO_TOTAL;
+            timerB = TEMPO_TOTAL;
+            pausado = true;
+            timerA_correndo = false; 
+            timerB_correndo = false;
+        }
+    }
+}
+
+void rotina_fim_de_tempo(){
+    if(timerA == 0 || timerB == 0){
+        // Ativar o buzzer para sinalizar o fim do tempo
+        pwm_set_gpio_level(BUZZER_PIN, 2048);
+        sleep_ms(1000); // O buzzer toca por 1 segundo
+        pwm_set_gpio_level(BUZZER_PIN, 0);
+
+        gpio_put(LED_R_PIN, 1);
+        gpio_put(LED_G_PIN, 0);
+        gpio_put(LED_B_PIN, 0);
+
+        if(timerA == 0){ // Se o timer A acabou, mostra no display que o jogador A perdeu
+            ssd1306_init();
+
+            struct render_area frame_area = {
+                start_column : 0,
+                end_column : ssd1306_width - 1,
+                start_page : 0,
+                end_page : ssd1306_n_pages - 1
+            };
+
+            calculate_render_area_buffer_length(&frame_area);
+
+            // Limpa o display
+            uint8_t ssd[ssd1306_buffer_length];
+            memset(ssd, 0, ssd1306_buffer_length);
+            render_on_display(ssd, &frame_area);
+
+            char *texto[] = {
+                "      ",
+                " ............ ",
+                "    ",
+                "    O TEMPO   ",
+                "    ACABOU!  ",
+                "      ",
+                "   A perdeu",};
+                
+
+            int y = 0;
+                for (uint i = 0; i < count_of(texto); i++)
+                {
+                    ssd1306_draw_string(ssd, 5, y, texto[i]);
+                    y += 8;
+                }
+                render_on_display(ssd, &frame_area);
+
+            sleep_ms(5000);
+        }
+        if(timerB == 0){ // Se o timer B acabou, mostra no display que o jogador B perdeu
+            ssd1306_init();
+
+            struct render_area frame_area = {
+                start_column : 0,
+                end_column : ssd1306_width - 1,
+                start_page : 0,
+                end_page : ssd1306_n_pages - 1
+            };
+
+            calculate_render_area_buffer_length(&frame_area);
+
+            // Limpa o display
+            uint8_t ssd[ssd1306_buffer_length];
+            memset(ssd, 0, ssd1306_buffer_length);
+            render_on_display(ssd, &frame_area);
+
+            char *texto[] = {
+                "      ",
+                " ............ ",
+                "    ",
+                "    O TEMPO   ",
+                "    ACABOU!  ",
+                "      ",
+                "   B perdeu",};
+
+
+            int y = 0;
+                for (uint i = 0; i < count_of(texto); i++)
+                {
+                    ssd1306_draw_string(ssd, 5, y, texto[i]);
+                    y += 8;
+                }
+                render_on_display(ssd, &frame_area);
+
+            sleep_ms(5000);
+            
+        }
+
+        timerA = TEMPO_TOTAL;
+        timerB = TEMPO_TOTAL;
+        pausado = true;
+        timerA_correndo = false; 
+        timerB_correndo = false;
+        gpio_put(LED_R_PIN, 0); // Desliga o led vermelho
+    }
+}
+
 int main()
 {
     stdio_init_all();
@@ -175,147 +317,10 @@ int main()
         bool b_apertado = gpio_get(BUTTON_B_PIN); // 1 solto, 0 apertado
         bool joystick_apertado = gpio_get(JOYSTICK_BUTTON_PIN); // 1 solto, 0 apertado
 
-    if(a_apertado == false){ // Se o botão A foi apertado
-            sleep_ms(10); // Tempo de debounce
-            if(a_apertado == false){
-            pausado = false;
-            timerA_correndo = false;
-            timerB_correndo = true;
-            gpio_put(LED_R_PIN, 0);
-            gpio_put(LED_G_PIN, 0);
-            gpio_put(LED_B_PIN, 1);
-        }
-    }
-    else if(b_apertado == false){
-            sleep_ms(10); // Tempo de debounce
-            if(b_apertado == false){
-            pausado = false;
-            timerA_correndo = true;
-            timerB_correndo = false;
-            gpio_put(LED_R_PIN, 0);
-            gpio_put(LED_G_PIN, 1);
-            gpio_put(LED_B_PIN, 0);
-        }
-    }
-    else if(joystick_apertado == false && pausado == false){
-            sleep_ms(10); // Tempo de debounce
-            if(joystick_apertado == false){
-            pausado = true;
-            timerA_correndo = false;
-            timerB_correndo = false;
-            gpio_put(LED_R_PIN, 0);
-            gpio_put(LED_G_PIN, 0);
-            gpio_put(LED_B_PIN, 0);
-        }
-    }
-    else if(joystick_apertado == false && pausado == true){ // Se o joystick foi apertado com o tempo pausado, o tempo é reiniciado;
-            sleep_ms(10); // Tempo de debounce
-            if(joystick_apertado == false && pausado == true){ 
-                timerA = TEMPO_TOTAL;
-                timerB = TEMPO_TOTAL;
-                pausado = true;
-                timerA_correndo = false; 
-                timerB_correndo = false;
-    
-        }
+        verifica_botoes(a_apertado, b_apertado, joystick_apertado);
+  
+        rotina_fim_de_tempo();
 
-    }
-
-        
-        if(timerA == 0 || timerB == 0){
-            // Ativar o buzzer para sinalizar o fim do tempo
-            pwm_set_gpio_level(BUZZER_PIN, 2048);
-            sleep_ms(1000); // O buzzer toca por 1 segundo
-            pwm_set_gpio_level(BUZZER_PIN, 0);
-
-            gpio_put(LED_R_PIN, 1);
-            gpio_put(LED_G_PIN, 0);
-            gpio_put(LED_B_PIN, 0);
-    
-            if(timerA == 0){ // Se o timer A acabou, mostra no display que o jogador A perdeu
-                ssd1306_init();
-    
-                struct render_area frame_area = {
-                    start_column : 0,
-                    end_column : ssd1306_width - 1,
-                    start_page : 0,
-                    end_page : ssd1306_n_pages - 1
-                };
-    
-                calculate_render_area_buffer_length(&frame_area);
-    
-                // Limpa o display
-                uint8_t ssd[ssd1306_buffer_length];
-                memset(ssd, 0, ssd1306_buffer_length);
-                render_on_display(ssd, &frame_area);
-    
-                char *texto[] = {
-                    "      ",
-                    " ............ ",
-                    "    ",
-                    "    O TEMPO   ",
-                    "    ACABOU!  ",
-                    "      ",
-                    "   A perdeu",};
-                    
-    
-                int y = 0;
-                for (uint i = 0; i < count_of(texto); i++)
-                {
-                    ssd1306_draw_string(ssd, 5, y, texto[i]);
-                    y += 8;
-                }
-                render_on_display(ssd, &frame_area);
-    
-                sleep_ms(5000);
-            }
-            if(timerB == 0){ // Se o timer B acabou, mostra no display que o jogador B perdeu
-                ssd1306_init();
-    
-                struct render_area frame_area = {
-                    start_column : 0,
-                    end_column : ssd1306_width - 1,
-                    start_page : 0,
-                    end_page : ssd1306_n_pages - 1
-                };
-    
-                calculate_render_area_buffer_length(&frame_area);
-    
-                // Limpa o display
-                uint8_t ssd[ssd1306_buffer_length];
-                memset(ssd, 0, ssd1306_buffer_length);
-                render_on_display(ssd, &frame_area);
-    
-                char *texto[] = {
-                    "      ",
-                    " ............ ",
-                    "    ",
-                    "    O TEMPO   ",
-                    "    ACABOU!  ",
-                    "      ",
-                    "   B perdeu",};
-
-    
-                int y = 0;
-                for (uint i = 0; i < count_of(texto); i++)
-                {
-                    ssd1306_draw_string(ssd, 5, y, texto[i]);
-                    y += 8;
-                }
-                render_on_display(ssd, &frame_area);
-    
-                sleep_ms(5000);
-                
-            }
-    
-            timerA = TEMPO_TOTAL;
-            timerB = TEMPO_TOTAL;
-            pausado = true;
-            timerA_correndo = false; 
-            timerB_correndo = false;
-            gpio_put(LED_R_PIN, 0); // Desliga o led vermelho
-        }
-
-        sleep_ms(1000);
+        sleep_ms(1000); // Para que a atualização seja feita a cada segundo
     }
 }
